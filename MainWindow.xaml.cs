86ab2570d42852db936fae71f5e079081d12a64b
dinar <dinar>
2023-10-15 20:56:54 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParallelPictureProcessing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapImage original;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            var fd = new OpenFileDialog();
+
+            fd.DefaultExt = "*.png";
+            fd.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|CMYK files (*.CMYK)|*.CMYK)";
+            
+            Nullable<bool> result = fd.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document 
+                string filename = fd.FileName;
+                original = new BitmapImage(new Uri(filename));
+                originalImg.Source = original;
+                format.Content = original.Format;
+            }
+
+        }
+
+        private void Process_Click(object sender, RoutedEventArgs e)
+        {
+            if (original is null) return;
+
+            Bitmap bitmap = new Bitmap(original.UriSource.AbsolutePath);
+            System.Drawing.Color cl = bitmap.GetPixel(0,670);
+            
+        }
+    }
+}
