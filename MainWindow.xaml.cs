a35f29619de000ac3cd47ca5447f215f248aa29d
dinar <dinar>
2023-10-23 23:55:37 +0700

    Added select and show specific channel of converted image

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net;
 using System.Net.Cache;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -35,6 +36,7 @@ namespace ParallelPictureProcessing
         BitmapImage original;
         Bitmap originalBmp;
         byte[] YCbCrBytes;
+        List<byte[]> picesOfYCbCrBytes;
 
         public MainWindow()
         {
@@ -44,8 +46,7 @@ namespace ParallelPictureProcessing
         {
             var fd = new OpenFileDialog();
 
-            fd.DefaultExt = "*.png";
-            fd.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|CMYK files (*.CMYK)|*.CMYK)";
+            fd.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp\"";
 
             Nullable<bool> result = fd.ShowDialog();
 
@@ -80,8 +81,10 @@ namespace ParallelPictureProcessing
 
             //Converting img to desire format
             var standardBitmapAndBytes = originalBmp.ToByte24BppRgbArray();
-            YCbCrBytes = standardBitmapAndBytes.Item1.ToYCbCrJpegFormat();
-            
+            var colors = standardBitmapAndBytes.Item1.ToYCbCrJpegFormat();
+            YCbCrBytes = colors[0];
+            picesOfYCbCrBytes = new List<byte[]>(colors.Skip(1));
+
             transformedImg.Source = Imaging.CreateBitmapSourceFromHBitmap(YCbCrBytes.ToBitmap(standardBitmapAndBytes.Item2.Width, standardBitmapAndBytes.Item2.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb).GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
         }
 
@@ -96,6 +99,15 @@ namespace ParallelPictureProcessing
                 Int32Rect.Empty, 
                 BitmapSizeOptions.FromEmptyOptions()
             );
+        }
+
+        private void ColorSpace_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (picesOfYCbCrBytes is null) return;
+
+            var select = sender as ComboBox;
+            var index = Convert.ToInt32((select.SelectedValue as ComboBoxItem).Content);
+            transformedImg.Source = Imaging.CreateBitmapSourceFromHBitmap(picesOfYCbCrBytes[index].ToBitmap((int) original.Width, (int) original.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb).GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
         }
     }
 
@@ -153,50 +165,81 @@ namespace ParallelPictureProcessing
             return nBitmap;
         }
 
-        public static byte[] ToYCbCrJpegFormat(this byte[] oBytes)
+        /// <summary>
+        /// Returns an array of transformed data with separated color instances
+        /// </summary>
+        public static List<byte[]> ToYCbCrJpegFormat(this byte[] oBytes)
         {
-            var nBytes = new byte[oBytes.Length];
+            var allTheBytes = new List<byte[]>();
 
-            for (int i = 0; i < oBytes.Length; ++i)//8 - a, 8 - b, 8 - r, 8 - g
+            var nBytes = new byte[oBytes.Length];
+            var YBytes = new byte[nBytes.Length];
+            var CbBytes = new byte[nBytes.Length];
+            var CrBytes = new byte[nBytes.Length];
+
+            for (int i = 0; i < oBytes.Length; i+=3 )//8 - a, 8 - b, 8 - r, 8 - g
             {
                 byte r = 0, g = 0, b = 0;
 
-                try { r = oBytes[i]; } catch { }
-                try { g = oBytes[i + 8]; } catch { }
-                try { b = oBytes[i + 16]; } catch { }
+                try { b = oBytes[i]; } catch { }
+                try { g = oBytes[i + 1]; } catch { }
+                try { r = oBytes[i + 2]; } catch { }
 
 
                 int Y = (int)( 0.299 * r + 0.587 * g + 0.114 * b),
-                    Cb = (int) (128 - 0.1168736 * r - 0.331264 * g + 0.5 * b),
+                    Cb = (int) (-0.1168736 * r - 0.331264 * g + 0.5 * b + 128),
                     Cr = (int) (128 + 0.5 * r - 0.418688 * g - 0.081312 * b);
 
                 Y = Math.Max(0, Math.Min(255, Y));
                 Cb = Math.Max(0, Math.Min(255, Cb));
                 Cr = Math.Max(0, Math.Min(255, Cr));
 
-                try { nBytes[i] = Convert.ToByte(Y); } catch { }
-                try { nBytes[i + 8] = Convert.ToByte(Cb); } catch { }
-                try { nBytes[i + 16] = Convert.ToByte(Cr); } catch { }
+                try { 
+                    nBytes[i] = (byte)Y; 
+                    YBytes[i] = (byte)Y;
+                    CrBytes[i] = 0;
+                    CbBytes[i] = 0;
+                } catch { }
+
+                try { 
+                    nBytes[i + 1] = (byte) Cb;
+                    CbBytes[i + 1] = (byte) Cb;
+                    YBytes[i + 1] = 0;
+                    CrBytes[i + 1] = 0;
+                } catch { }
+
+                try { 
+                    nBytes[i + 2] = (byte) Cr;
+                    CrBytes[i + 2] = (byte) Cr;
+                    CbBytes[i + 2] = 0;
+                    YBytes[i + 2] = 0; 
+                } catch { }
+
             }
 
-            return nBytes;
+            allTheBytes.Add(nBytes);//Transformed bytes
+            allTheBytes.Add(YBytes);
+            allTheBytes.Add(CbBytes);
+            allTheBytes.Add(CrBytes);
+
+            return allTheBytes;
         }
 
         public static byte[] ToRgbFromYCbCrJpegFormat(this byte[] oBytes)
         {
             var nBytes = new byte[oBytes.Length];
 
-            for (int i = 0; i < oBytes.Length; ++i)//8 - a, 8 - b, 8 - r, 8 - g
+            for (int i = 0; i < oBytes.Length; i+=3)//8 - a, 8 - b, 8 - r, 8 - g
             {
                 byte y = 0, cr = 0, cb = 0;
 
                 try { y = oBytes[i]; } catch { }
-                try { cr = oBytes[i + 8]; } catch { }
-                try { cb = oBytes[i + 16]; } catch { }
+                try { cr = oBytes[i + 1]; } catch { }
+                try { cb = oBytes[i + 2]; } catch { }
 
 
                 int r = (int) (y + 1.402 * (cr - 128)),
-                    g = (int) (y - 0.34414 * (cb - 128) - 0.71414 * (cr -128)),
+                    g = (int) (y - 0.344136 * (cb - 128) - 0.714136 * (cr -128)),
                     b = (int)(y + 1.772 * (cb - 128));
 
                 r = Math.Max(0, Math.Min(255, r));
@@ -204,8 +247,8 @@ namespace ParallelPictureProcessing
                 b = Math.Max(0, Math.Min(255, b));
 
                 try { nBytes[i] = Convert.ToByte(r); } catch { }
-                try { nBytes[i + 8] = Convert.ToByte(g); } catch { }
-                try { nBytes[i + 16] = Convert.ToByte(b); } catch { }
+                try { nBytes[i + 1] = Convert.ToByte(g); } catch { }
+                try { nBytes[i + 2] = Convert.ToByte(b); } catch { }
             }
 
             return nBytes;
